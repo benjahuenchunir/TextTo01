2832651ccd14b11a20c3940955661805636e90e2
Benja <bahuenchunir@gmail.com>
2025-05-28 16:00:10 -0400

    fix: disable drag on grid

--- src/components/GridSelection.tsx
+++ src/components/GridSelection.tsx
@@ -27,21 +27,27 @@ export default function GridSelection(props: {
   const handleMouseUp = () => setIsSelecting(false)
 
   return (
-    <div className="flex flex-col" onMouseUp={handleMouseUp} onTouchEnd={handleMouseUp}>
+    <div 
+      className="flex flex-col select-none touch-none" 
+      onMouseUp={handleMouseUp} 
+      onTouchEnd={handleMouseUp}
+    >
       {grid.map((row, rowIndex) => (
         <div className="flex flex-row" key={rowIndex}>
           {row.map((isActive, colIndex) => (
             <div
               key={`${rowIndex}-${colIndex}`}
-              className={`w-3 h-3 md:w-4 md:h-4 cursor-pointer ${
+              className={`w-3 h-3 md:w-4 md:h-4 cursor-pointer select-none touch-none ${
                 isActive 
                   ? 'bg-green-500 dark:bg-green-600' 
                   : 'bg-red-500 dark:bg-red-700'
               }`}
+              draggable={false}
               onMouseDown={handleSquareInteraction(rowIndex, colIndex)}
               onMouseEnter={handleSquareMouseEnter(rowIndex, colIndex)}
               onTouchStart={handleSquareInteraction(rowIndex, colIndex)}
               onTouchMove={handleSquareMouseEnter(rowIndex, colIndex)}
+              onDragStart={(e) => e.preventDefault()}
             />
           ))}
         </div>
